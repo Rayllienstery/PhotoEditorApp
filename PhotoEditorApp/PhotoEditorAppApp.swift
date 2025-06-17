2744ef814ce808d4691012750fdb485d3e7ede41
Kostiantyn Kolosov <rayleefx@protonmail.com>
2025-06-17 17:54:47 +0300

    Initial Commit

---
+++ PhotoEditorApp/PhotoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppApp.swift
+//  PhotoEditorApp
+//
+//  Created by Kostiantyn Kolosov on 17.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
